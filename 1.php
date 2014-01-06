5b9226bb76371498ffd3bb60827a3985b2f8b32d
Jonathan Klein <jonathan.n.klein@gmail.com>
2014-01-06 12:07:32 -0500

    Stubbing out first question

--- 1.php
+++ 1.php
@@ -2,5 +2,12 @@
 
 require 'phplib/bootstrap.php';
 
+if ($_POST['answer'] == 'correct answer') {
+    $success = true;
+}
+
 $tpl = $mustache->loadTemplate('1');
-echo $tpl->render(array('bar' => 'baz'));
+echo $tpl->render(array(
+  'bar' => 'baz',
+  'success' => $success,
+));
